055427ad3452d260b72480a5f64f7c69ec45be85
Tom Augspurger <taugspurger@microsoft.com>
2021-10-19 06:53:28 -0500

    Fixup other tests for env

--- kbatch-server/app/test_app.py
+++ kbatch-server/app/test_app.py
@@ -77,6 +77,7 @@ class TestKBatch:
         assert result.pop("id")
         assert result.pop("username") == USERNAME
         assert result.pop("script") is None
+        assert result.pop("env") is None
 
         assert result == data
 
@@ -94,6 +95,7 @@ class TestKBatch:
         assert result.pop("id")
         assert result.pop("username") == USERNAME
         assert result.pop("command") is None
+        assert result.pop("env") is None
 
         assert result == data
 
