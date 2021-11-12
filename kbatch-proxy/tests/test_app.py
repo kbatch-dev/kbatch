fab973e8fff12e1d9e1b74e909cfff7bf0a92be5
Tom Augspurger <taugspurger@microsoft.com>
2021-11-12 08:57:27 -0600

    more logs

--- kbatch-proxy/tests/test_app.py
+++ kbatch-proxy/tests/test_app.py
@@ -18,7 +18,7 @@ def mock_hub_auth(mocker):
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "kbatch"}
+    assert response.json() == {"message": "kbatch-prefix"}
 
 
 @pytest.mark.usefixtures("mock_hub_auth")
