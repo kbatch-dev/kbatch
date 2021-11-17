09822fd8620992ad7f678469190c204422cff660
Tom Augspurger <taugspurger@microsoft.com>
2021-11-17 09:39:22 -0600

    RLS: 0.2.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Tom Augspurger"
 author = "Tom Augspurger"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.7"
+release = "0.2.8"
 
 
 # -- General configuration ---------------------------------------------------
