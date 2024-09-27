551cbc119082103e13ab9dd50ca12ecd426dcd75
Min RK <benjaminrk@gmail.com>
2024-09-27 16:08:26 +0200

    Bump to 0.5.0.dev0
    (tbump test)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Tom Augspurger"
 author = "Tom Augspurger"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.2"
+release = "0.5.0.dev0"
 
 
 # -- General configuration ---------------------------------------------------
